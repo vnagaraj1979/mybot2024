9531d12a96f741ec00c49666b0d0bdb839a145b7
vnagaraj1979 <nvankam@yahoo.com>
2024-11-30 16:52:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # Title of the app
-st.title("My Vankam ChatBOT")
+st.title("My ChatBOT")
 
 # Initialize session state for chat history
 if "messages" not in st.session_state:
